1129b21672b87995fc432939d646b248e7cdba12
stesho01 <Listo4ek>
2021-12-04 23:00:41 +0200

    Add task 2

---
+++ src/com/example/java/Main.java
@@ -0,0 +1,12 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(-5+8*6);
+        System.out.println((55+9)%9);
+        System.out.println(20 + -3*5/8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+    }
+}
